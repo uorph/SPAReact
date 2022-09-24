9189d913646ba8c845dbca51e109afbc7e2f6a82
Regina Richardson <reginarichardson@Robot03.local>
2022-09-23 18:32:40 -0700

    Staging first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'; 
+function App(){
+    return <h1>Regina Lilly Richardson</h1>
+}
+const container = document.getElementById('app'); 
+const root = ReactDOM.createRoot(container); 
+root.render(<App />); 
